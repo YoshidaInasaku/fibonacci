444798b4a307bca16c3d05c9919624f7c68b9dcd
吉田稲作 <yoshidanoinasaku@gmail.com>
2020-05-28 20:57:57 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 'use strict';
+const memo = new Map();    // マップを作成（連想配列を作成）
+memo.set(0, 0);    // キー値と値をセット
+memo.set(1, 1);    //  この場合だと「0は値0」「1は値1」
 function fib(n) {
-    if(n === 0) {
-        return 0;
-    } else if (n === 1) {
-        return 1
+    // もしマップがnをキーとした値を持っていたら、nを返す
+    if(memo.has(n)) {
+        return memo.get(n);
     }
-    return fib(n - 1) + fib(n - 2);
+    const value = fib(n - 1) + fib(n - 2);
+    memo.set(n, value);
+    return value;
 }
 const length = 40;
-for(let i = 0; i < length; i++) {
+for(let i = 0; i <= length; i++) {
     console.log(fib(i));
 }
